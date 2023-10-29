65c53f975f694c8f6f6c63e39ebf8815a2e11cf8
rushi <hrushikeshmysala2003@gmail.com>
2023-10-29 10:55:05 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.7",
+};
+// npm install @nomiclabs/hardhat-ethers hardhat-deploy-ethers ethers @nomiclabs/hardhat-etherscan @nomiclabs/hardhat-waffle chai ethereum-waffle hardhat hardhat-contract-sizer hardhat-deploy hardhat-gas-reporter prettier prettier-plugin-solidity solhint solidity-coverage dotenv
